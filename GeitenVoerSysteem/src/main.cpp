2e2b7eb85f3824a9253652fc22deb440dd54a8a1
Brent Brouwer <brentbrouwer13@gmail.com>
2024-11-16 20:59:39 +0100

    led controll working, preprocessor still not

--- GeitenVoerSysteem/src/main.cpp
+++ GeitenVoerSysteem/src/main.cpp
@@ -1,17 +1,15 @@
 #include <Arduino.h>
 #include "Board/ESP32_Board.h"
 
-
 // Hardware
 static ESP32_Board s_Board;
 
 void setup()
 {
-    
 }
 
 void loop()
 {
-    // s_Board.StatusLedControl();
+    s_Board.StatusLedControl();
     delay(10);
 }
