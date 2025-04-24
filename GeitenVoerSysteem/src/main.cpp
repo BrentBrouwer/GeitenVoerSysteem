c90a98ad361f3dfacfd358a1a1d2c2eba5463af1
Lenovo\brent <brentbrouwer13@gmail.com>
2025-04-24 18:37:30 +0200

    status led working

--- GeitenVoerSysteem/src/main.cpp
+++ GeitenVoerSysteem/src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Properties
+#define STATUS_LED_PIN 2
+int m_LastStatusChange = 0;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// put function declarations here:
+void LedStatusControl();
+
+void setup()
+{
+    pinMode(STATUS_LED_PIN, OUTPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    LedStatusControl();
+    sleep(1);
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void LedStatusControl()
+{
+    int now = millis();
+    if ((now - m_LastStatusChange) > 500)
+    {
+        m_LastStatusChange = now;
+        digitalWrite(STATUS_LED_PIN, !digitalRead(STATUS_LED_PIN));
+    }
 }
